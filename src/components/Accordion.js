c33b21566154e3ccf245cb36f552013a7c165b70
JuniorBaymax <cooladitya953@gmail.com>
2020-09-11 00:47:48 +0530

    added Hooks state to functional component

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Accordion({ items }) {
-  const renderedItems = items.map((item) => {
+  const [activeState, setActiveState] = useState(null);
+
+  const onTitleClick = (index) => {
+    setActiveState(index);
+  };
+
+  const renderedItems = items.map((item, index) => {
+    
+    const active = index === activeState ? 'active' : '';
     return (
       <React.Fragment key={item.title}>
-        <div className="title active">
+        <div className={`title ${active}`} onClick={() => onTitleClick(index)}>
           <i className="dropdown icon"></i>
           {item.title}
         </div>
-        <div className="content active">
+        <div className={`content ${active}`}>
           <p>{item.content}</p>
         </div>
       </React.Fragment>
     );
   });
-  return <div className="ui styled accordion">{renderedItems}</div>;
+  return <div className="ui styled accordion">{renderedItems}
+  </div>;
 }
