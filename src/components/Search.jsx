ffa049aea3f5ffca6b183a38d851e861ad525872
JuniorBaymax <cooladitya953@gmail.com>
2020-09-11 08:23:40 +0530

    Added Search Widgets Component to project

---
+++ src/components/Search.jsx
@@ -0,0 +1,20 @@
+import React, {useState} from 'react'
+
+ const Search = () => {
+     const [term, setTerm] = useState('')
+    return (
+        <div>
+            <div className="ui form">
+                <div className="field">
+                    <label >Enter Search Term</label>
+                    <input type="search" className='input'
+                        value={term}
+                        onChange={e => setTerm(e.target.value)}
+                    />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Search;
