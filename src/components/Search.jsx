581ddb994d4260332d61a5909983e6e2b816ab38
JuniorBaymax <cooladitya953@gmail.com>
2020-09-11 22:03:15 +0530

    Added axios Support

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,15 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react';
+import axios from "axios";
 
  const Search = () => {
      const [term, setTerm] = useState('')
+     
+     /**
+      * !Adding lifeCycle method to functional component
+      */
+      useEffect(()=> {
+      },[term])
+      
     return (
         <div>
             <div className="ui form">
