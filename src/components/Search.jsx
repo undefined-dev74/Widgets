6621100226647bc50b9229fef1915b7fe3cf713f
JuniorBaymax <cooladitya953@gmail.com>
2020-09-11 22:22:56 +0530

    Added Wikipedia API Suppot

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,28 +1,41 @@
-import React, {useState, useEffect} from 'react';
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
- const Search = () => {
-     const [term, setTerm] = useState('')
-     
-     /**
-      * !Adding lifeCycle method to functional component
-      */
-      useEffect(()=> {
-      },[term])
-      
-    return (
-        <div>
-            <div className="ui form">
-                <div className="field">
-                    <label >Enter Search Term</label>
-                    <input type="search" className='input'
-                        value={term}
-                        onChange={e => setTerm(e.target.value)}
-                    />
-                </div>
-            </div>
+const Search = () => {
+  const [term, setTerm] = useState('');
+
+  /**
+   * !Adding lifeCycle method to functional component
+   */
+  useEffect(() => {
+    (async () => {
+      await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: term,
+        },
+      });
+    })();
+  }, [term]);
+
+  return (
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter Search Term</label>
+          <input
+            type="search"
+            className="input"
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
+          />
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Search;
