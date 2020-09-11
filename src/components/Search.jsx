c12071bef3d66eab13d60d8c2a636ef5a170361e
JuniorBaymax <cooladitya953@gmail.com>
2020-09-12 00:27:11 +0530

    Complete the search with API

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,14 +2,15 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Search = () => {
-  const [term, setTerm] = useState('');
-
+  const [term, setTerm] = useState('Wikipedia Home');
+  const [results, setResults] = useState([]);
   /**
    * !Adding lifeCycle method to functional component
    */
+  console.log(results);
   useEffect(() => {
-    (async () => {
-      await axios.get('https://en.wikipedia.org/w/api.php', {
+    const search = async () => {
+      const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
         params: {
           action: 'query',
           list: 'search',
@@ -18,9 +19,35 @@ const Search = () => {
           srsearch: term,
         },
       });
-    })();
+      setResults(data.query.search);
+    };
+    
+    setTimeout(() => {
+      if (term) {
+        search();
+      }
+    }, 1000);
+    
   }, [term]);
 
+  const renderedResults = results.map((result) => {
+    return (
+      <div key={result.pageid} className="item">
+        <div className="content">
+          <div className="right floated content">
+            <a
+              className="ui button"
+              href={`https://en.wikepedia.org?curid=${result.pageid}`}
+            >
+              Go
+            </a>
+          </div>
+          <div className="header">{result.title}</div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+        </div>
+      </div>
+    );
+  });
   return (
     <div>
       <div className="ui form">
@@ -34,6 +61,7 @@ const Search = () => {
           />
         </div>
       </div>
+      <div className="ui celled list">{renderedResults}</div>
     </div>
   );
 };
