834bde30df7ef0779dd93b5b87d78f64da5d7880
JuniorBaymax <cooladitya953@gmail.com>
2020-09-18 00:20:04 +0530

    Displayed translated text to the App

--- src/components/Convert.jsx
+++ src/components/Convert.jsx
@@ -1,16 +1,29 @@
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 
-const Convert = ({language, text} ) => {
-    
-    useEffect(() => {
-        axios.post('https://translation.googleapis.com/language/translation/v2')
-    }, [language, text]);
-    return (
-        <div>
-            
-        </div>
-    )
-}
+const Convert = ({ language, text }) => {
+  const [translated, setTranslated] = useState('');
+  useEffect(() => {
+    const doTranslation = async () => {
+      const { data } = await axios.post(
+        'https://translation.googleapis.com/language/translate/v2',
+        {},
+        {
+          params: {
+            q: text,
+            target: language.value,
+            key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM',
+          },
+        }
+      );
+
+      setTranslated(data.data.translations[0].translatedText);
+    };
+    doTranslation();
+  }, [language, text]);
+  return <div>
+      <h1 className="ui header">{translated}</h1>
+  </div>;
+};
 
 export default Convert;
